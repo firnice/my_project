adb09c6c05293e404cef850e5af964457c6be64c
liyiliang <lyl120751@alibaba-inc.com>
2018-03-19 16:37:31 +0800

    push

--- mp-test-two/src/main/java/com/cornerstone/leetcode/Solution.java
+++ mp-test-two/src/main/java/com/cornerstone/leetcode/Solution.java
@@ -13,11 +13,11 @@ class Solution {
         System.out.println(ints[1]);
     }
 
-    public static int[]  twoSum(int[] nums, int target) {
-        for(int i = 0;i<nums.length-1;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
                 }
             }
         }
